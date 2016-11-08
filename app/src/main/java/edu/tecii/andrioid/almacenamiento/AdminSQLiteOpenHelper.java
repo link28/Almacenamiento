edf6355e8ce1b330e9d347a85fbb7161d5792e4a
link28 <link_muaries@hotmail.com>
2016-11-07 22:58:25 -0700

    Initial commit almacenamiento

---
+++ app/src/main/java/edu/tecii/andrioid/almacenamiento/AdminSQLiteOpenHelper.java
@@ -0,0 +1,33 @@
+package edu.tecii.andrioid.almacenamiento;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by oswald on 7/11/2016.
+ */
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                                 int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table articulos(codigo int primary key,descripcion text,precio real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+
+
+
+
+
+
+}
